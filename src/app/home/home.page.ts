e507a0f32b55aab17601dd50c7be0bc110587f00
Madhup <madhup@jixtra.com>
2020-06-22 20:48:13 +0530

    Updating Current String on Button Clicks

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,28 +6,42 @@ import { Component } from '@angular/core';
     styleUrls: [ 'home.page.scss' ],
 })
 export class HomePage {
-    currentString: String = "Hello";
+    resultString: String = '';
+    currentString: String = '';
+    currentOperation: String = '';
 
     constructor() { }
 
     reset = () => {
         console.log("Performing Reset");
+        this.currentString = '';
+        this.resultString = '';
     }
 
     delete = () => {
         console.log("Performing Backspace");
+        if (this.currentString.length > 0) {
+            this.currentString = this.currentString.substr(0, this.currentString.length - 1);
+        } else {
+            this.reset();
+        }
     }
 
     perform = (operation) => {
         console.log(`Performing ${ operation }`);
+        this.currentOperation = operation;
     }
 
     numberClicked = (number) => {
         console.log(`Number ${ number } pressed.`);
+        this.currentString = this.currentString + number;
     }
 
     decimalClicked = () => {
         console.log("Decimal Clicked");
+        if (this.currentString.indexOf('.') < 0) {
+            this.currentString = this.currentString + '.';
+        }
     }
 
 }
