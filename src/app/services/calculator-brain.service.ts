1034104750964c47caedc48a6af1075dade0c500
Madhup <madhup@jixtra.com>
2020-06-23 19:47:13 +0530

    Renaming And Changin Data Types of Variables

--- src/app/services/calculator-brain.service.ts
+++ src/app/services/calculator-brain.service.ts
@@ -5,51 +5,60 @@ import { Injectable } from '@angular/core';
 })
 export class CalculatorBrainService {
 
-    private resultString: string = '';
-    currentString: string = '';
+    private firstInput: number = null;
+    displayString: string = '';
     private currentOperation: string = '';
 
     constructor() { }
 
     public reset = () => {
         console.log("Performing Reset");
-        this.currentString = '';
-        this.resultString = '';
+        this.displayString = '';
+        this.firstInput = null;
         this.currentOperation = '';
     }
 
     public delete = () => {
         console.log("Performing Backspace");
-        if (this.currentString.length > 0) {
-            this.currentString = this.currentString.substr(0, this.currentString.length - 1);
+        if (this.displayString.length > 0) {
+            this.displayString = this.displayString.substr(0, this.displayString.length - 1);
         } else {
             this.reset();
         }
     }
 
-    public percentage = (inValue: string) => {
+    private percentage = (inValue: string) => {
         const result = parseFloat(inValue) / 100.0;
         return `${ result.toFixed(2) }`;
+    }
+
+    private evaluate = () => {
+        if (this.firstInput != null) {
+
+        }
     }
 
     public perform = (operation) => {
         console.log(`Performing ${ operation }`);
         if (operation == '%') {
-            this.currentString = this.percentage(this.currentString);
+            this.displayString = this.percentage(this.displayString);
         } else {
+            if (this.currentOperation != null) {
+                this.evaluate();
+            }
             this.currentOperation = operation;
         }
     }
 
     public numberClicked = (number) => {
         console.log(`Number ${ number } pressed.`);
-        this.currentString = this.currentString + number;
+        this.displayString = this.displayString + number;
     }
 
     public decimalClicked = () => {
         console.log("Decimal Clicked");
-        if (this.currentString.indexOf('.') < 0) {
-            this.currentString = this.currentString + '.';
+        if (this.displayString.indexOf('.') < 0) {
+            this.displayString = this.displayString + '.';
         }
     }
 }
